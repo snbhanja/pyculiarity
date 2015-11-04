5a3e3fd869ccc9fa88a470d95f1522d479f52351
Will Mcginnis <will@predikto.com>
2015-11-04 14:50:02 -0500

    updating setup and reqs

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 """
-Usage details and source available here: https://github.com/nicolasmiller/pyculiarity.
+Usage details and source available here: https://github.com/wdm0006/pyculiarity.
 
 The original R source and examples are available here: https://github.com/twitter/AnomalyDetection.
 
 Copyright and License
 
-Python port Copyright 2015 Nicolas Steven Miller
+Original Python port Copyright 2015 Nicolas Steven Miller
 
 Original R source Copyright 2015 Twitter, Inc and other contributors
 
 Licensed under the GPLv3
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+VERSION = '0.0.2'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' in x]
 
 setup(
     name='pyculiarity',
@@ -23,7 +40,6 @@ setup(
     author='Nicolas Steven Miller',
     author_email='nicolasmiller@gmail.com',
     license='GPL',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
@@ -33,8 +49,8 @@ setup(
     ],
     keywords='data anomaly detection pandas timeseries',
     packages=['pyculiarity'],
-    install_requires=['numpy', 'scipy', 'pandas', 'pytz',
-                      'statsmodels'],
+    install_requires=install_requires,
+    depedency_links=dependency_links,
     extras_require={
         'test': ['nose', 'mock']
     }
